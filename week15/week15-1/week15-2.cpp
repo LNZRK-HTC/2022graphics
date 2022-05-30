de429b8086a7e2c7b76ea8a392b4660f9dc2270a
unknown <LNZRK531126@gmail.com>
2022-05-30 17:47:33 +0800

    上傳第15週

---
+++ week15/week15-1/week15-2.cpp
@@ -0,0 +1,12 @@
+///week15-2 SND_SYNC璶单˙ VS.
+///SND_ASYNCぃノ单ぃ˙
+#include <windows.h>
+#include <stdio.h>///printf()
+int main()
+{
+    printf("瞷PlaySound()玡\n");
+    PlaySound("07042111.wav",NULL,SND_ASYNC);
+    printf("瞷PlaySiund()\n");
+    int N;
+    scanf("%d",&N);///单块计
+}
